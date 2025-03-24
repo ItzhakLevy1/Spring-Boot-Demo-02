158e5b48b89f9c29f52de44e116beb9fb961463c
Itzhak Levy <itzhaklevydev@gmail.com>
2025-03-24 11:15:25 +0200

    Bean lifecycle methods, @PostConstruct / @PreDestroy

--- 01-constructor-injection/src/main/java/com/love2code/springfcoredemo/CricketCoach.java
+++ 01-constructor-injection/src/main/java/com/love2code/springfcoredemo/CricketCoach.java
@@ -1,5 +1,7 @@
 package com.love2code.springfcoredemo;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component  // Marks this class as a Spring bean and makes it available for dependency injection
@@ -8,6 +10,19 @@ public class CricketCoach implements Coach{
     public CricketCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
     }
+
+    // An init method - this method will be called after the constructor and after the dependencies have been injected (Once the application starts this will show in the terminal)
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    // A destroy method - this method will be called before the bean is destroyed (Once the application is terminated this will show in the terminal)
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
+    }
+
 
     @Override
     public String getDailyWorkout() {
