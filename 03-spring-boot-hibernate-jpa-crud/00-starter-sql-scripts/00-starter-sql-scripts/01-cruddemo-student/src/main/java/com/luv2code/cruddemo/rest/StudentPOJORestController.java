8b76f2ab324b7bb34b566c7d53dffe90a90d300b
Itzhak Levy <itzhaklevydev@gmail.com>
2025-04-03 15:55:19 +0300

    Rest path variable - retrieve a specific student based on its studentId / index

--- 03-spring-boot-hibernate-jpa-crud/00-starter-sql-scripts/00-starter-sql-scripts/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/rest/StudentPOJORestController.java
+++ 03-spring-boot-hibernate-jpa-crud/00-starter-sql-scripts/00-starter-sql-scripts/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/rest/StudentPOJORestController.java
@@ -1,7 +1,10 @@
 package com.luv2code.cruddemo.rest;
 
+import com.luv2code.cruddemo.entity.Student;
 import com.luv2code.cruddemo.entity.StudentPOJO;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +15,27 @@ import java.util.List;
 @RequestMapping("/api") // This annotation maps HTTP requests to the specified URL path
 public class StudentPOJORestController {
 
+    private List<StudentPOJO> theStudents; // This theStudents variable holds the list of students
+
+    @PostConstruct  // Indicates that the method should run automatically after the bean is created and initialized, but before it is used
+    public void loadData() {
+
+        theStudents = new ArrayList<>(); // Create a new list of students
+
+        theStudents.add(new StudentPOJO("Poornima", "Patel")); // Add a new student to the list
+        theStudents.add(new StudentPOJO("Mario", "Rossi")); // Add a new student to the list
+        theStudents.add(new StudentPOJO("Mary", "Smith")); // Add a new student to the list
+    }
 
     @GetMapping("/students") // This annotation maps HTTP GET requests to the "/students" endpoint
     public List<StudentPOJO> getStudents() { // This method returns a list of students ( POJO stands for Plain Old Java Object )
-
-        List<StudentPOJO> theStudents = new ArrayList<>(); // Create a new list of students
-
-        theStudents.add(new StudentPOJO("Poorima", "Patel")); // Add a new student to the list
-        theStudents.add(new StudentPOJO("Mario", "Rossi")); // Add a new student to the list
-        theStudents.add(new StudentPOJO("Mary", "Smith")); // Add a new student to the list
-
         return theStudents;
+    }
+
+    // An endpoint to get a student at index - "/students/{studentId}"
+    // The full path would be: http://localhost:8080/api/students/0
+    @GetMapping("/students/{studentId}")
+    public StudentPOJO getStudent(@PathVariable int studentId) { // This method returns a student at the specified index, @PathVariable is used to extract the value from the URL
+        return theStudents.get(studentId); // Return the student at the specified index
     }
 }
