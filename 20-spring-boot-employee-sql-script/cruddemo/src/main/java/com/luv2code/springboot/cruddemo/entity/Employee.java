c6f753488d788152c710fc3931f291f614bbb268
Itzhak Levy <itzhaklevydev@gmail.com>
2025-04-07 08:04:04 +0300

    Create a new project and connect it to the MySQL database

---
+++ 20-spring-boot-employee-sql-script/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.cruddemo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+@Entity // An entity class representing an employee to be mapped to a database table
+@Table(name="employee")  // Specifies the table name in the database
+
+public class Employee {
+
+
+}
