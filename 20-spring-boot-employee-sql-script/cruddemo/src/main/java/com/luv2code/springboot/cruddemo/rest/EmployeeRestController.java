b860078bd3cf02d1ca6f172420451708b423140f
Itzhak Levy <itzhaklevydev@gmail.com>
2025-04-15 22:00:10 +0300

    Patch - Partially update an existing employee

--- 20-spring-boot-employee-sql-script/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 20-spring-boot-employee-sql-script/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -1,11 +1,13 @@
 package com.luv2code.springboot.cruddemo.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import java.util.Map;
 
 
 @RestController // This annotation indicates that this class is a REST controller and tells Spring: “This class handles HTTP requests and responses”
@@ -13,11 +15,13 @@ import java.util.List;
 public class EmployeeRestController {
 
     private EmployeeService employeeService; // Service layer to handle business logic
+    private ObjectMapper objectMapper; // ObjectMapper to convert Java objects to JSON and vice versa
 
     // Inject the Employee DAO using constructor injection
     @Autowired  // Constructor injection
-    public EmployeeRestController(EmployeeService theEmployeeService) {
+    public EmployeeRestController(EmployeeService theEmployeeService, ObjectMapper theObjectMapper) {
         this.employeeService = theEmployeeService;
+        this.objectMapper = theObjectMapper;
     }
 
     // Expose the "/employees" endpoint to get all employees
@@ -29,7 +33,7 @@ public class EmployeeRestController {
 
     // Add mapping for GET /employees/{employeeId}, The whole URL will be http://localhost:8080/api/employees/{employeeId}
     @GetMapping("/employees/{employeeId}") // This annotation maps HTTP GET requests to the findById() method
-    public Employee getEmployee(@PathVariable int employeeId) { // @PathVariable -  tells Spring to bind the URL path variable to the method parameter
+    public Employee getEmployee(@PathVariable int employeeId) { // @PathVariable -  tells Spring to bind the URL path variable ( employeeId ) to the method parameter
         Employee theEmployee = employeeService.findById(employeeId); // Call the DAO method to get the employee by ID
 
         if (theEmployee == null) {
@@ -41,6 +45,7 @@ public class EmployeeRestController {
 
 
     // Add mapping for POST /employees - add a new employee, the whole URL will be http://localhost:8080/api/employees
+
     /*
     The post request will contain the employee object in JSON format :
     {"firstName" : "Hector","lastName": "Perez","email" : "hector@luv2code.com"}
@@ -60,6 +65,7 @@ public class EmployeeRestController {
 
 
     // Add mapping for PUT /employees - update an existing employee, the whole URL will be http://localhost:8080/api/employees/{employeeId}
+
     /*
     The PUT request will contain the employee object in JSON format :
     {"id": 1,"firstName" : "Timothy","lastName": "Patterson","email" : "tim@luv2code.com"}
@@ -70,6 +76,43 @@ public class EmployeeRestController {
         Employee dbEmployee = employeeService.save(theEmployee); // Call the DAO method to save the employee
 
         return dbEmployee; // Return the updated employee object
+    }
+
+
+    // Add mapping for PATCH /employees - partially update an existing employee, the whole URL will be http://localhost:8080/api/employees/{employeeId}
+    @PatchMapping("/employees/{employeeId}") // This annotation maps HTTP PATCH requests to the partialUpdate() method
+    public Employee patchEmployee(@PathVariable int employeeId, // @PathVariable -  tells Spring to bind the URL path variable ( employeeId ) to the method parameter
+                                  @RequestBody Map<String, Object> patchPayload) {  // JSON data is passed in as a Map of key - value pairs
+
+        Employee tempEmployee = employeeService.findById(employeeId); // Call the DAO method to get the employee by ID
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId); // If the employee is not found, throw an exception
+        }
+
+        if (patchPayload.containsKey("id")) {
+            throw new RuntimeException("Employee id is not allowed in request body - " + employeeId); // If the employee ID is in the payload, throw an exception since we don't want to allow it to be updated
+        }
+
+        Employee patchedEmployee = apply(patchPayload, tempEmployee); // Apply the patch to the employee object ( Only in memory, not in the database yet )
+
+        Employee dbEmployee = employeeService.save(patchedEmployee); // Call the DAO method to save the employee ( This will update the employee in the database )
+
+        return dbEmployee; // Return the updated employee object
+    }
+
+    private Employee apply(Map<String, Object> patchPayload, Employee tempEmployee) {
+
+        // 1.Convert employee object to JSON object node ( string )
+        ObjectNode employeeNode = objectMapper.convertValue(tempEmployee, ObjectNode.class);
+
+        // 2.Convert the patch payload to JSON object node ( string )
+        ObjectNode patchNode = objectMapper.convertValue(patchPayload, ObjectNode.class);
+
+        // 3.Merge the patch node into the employee node
+        employeeNode.setAll(patchNode);
+
+        return objectMapper.convertValue(employeeNode, Employee.class); // Convert the merged node back to an Employee object
     }
 
 }
