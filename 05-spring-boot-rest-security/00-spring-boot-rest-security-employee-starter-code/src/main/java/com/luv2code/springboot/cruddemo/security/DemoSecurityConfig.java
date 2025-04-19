0322c4fce791faa421f44876eb5ead6606a5c6f0
Itzhak Levy <itzhaklevydev@gmail.com>
2025-04-19 20:14:24 +0300

    Restrict access based on roles - CRUD operations

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -31,6 +35,27 @@ public class DemoSecurityConfig {
                 .build();   // Create that user details instance
 
         return new InMemoryUserDetailsManager(john, mary, susan); // Create the in-memory user details manager with the users
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { // Create a bean of type SecurityFilterChain to configure security for the application
+
+        http.authorizeHttpRequests(configurer ->    // Configure the HTTP security
+                        configurer
+                                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE") // Allow access to /api/employees only to users with role EMPLOYEE
+                                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE") // Allow access to /api/employees/** only to users with role EMPLOYEE
+                                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")  // Allow access to /api/employees only to users with role MANAGER
+                                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")  // Allow access to /api/employees only to users with role MANAGER
+                                .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")  // Allow access to /api/employees only to users with role MANAGER
+                                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")  // Allow access to /api/employees/** only to users with role ADMIN
+        );
+        // Use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // Disable Cross-Site Request Forgery ( CSRF ) protection, in a REST API, CSRF protection is not needed for stateless APIs that use POST, PUT, DELETE, PATCH methods
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build(); // Build the security filter chain
     }
 }
 
